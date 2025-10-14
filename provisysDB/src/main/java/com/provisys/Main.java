ceb17766d012479142911915ba54304c53a7b310
Gari <garikworkaddress@gmail.com>
2025-10-14 13:33:49 +0200

    conexion a la base de datos (provisysDB)

--- provisysDB/src/main/java/com/provisys/Main.java
+++ provisysDB/src/main/java/com/provisys/Main.java
@@ -1,7 +1,22 @@
 package com.provisys;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USER = "odoo";
+    private static final String PASSWORD = "odoo";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            if (conn != null) {
+                System.out.println("Conexión exitosa a la base de datos PostgreSQL.");
+            }
+        } catch (SQLException e) {
+            System.out.println("KILL YOURSELF:");
+            e.printStackTrace();
+        }
     }
 }
